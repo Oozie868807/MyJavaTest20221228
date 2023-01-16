9b3205d282ab32876e050562e44897b092fd1a96
Oozie <goodMorning_glb@zhangjian.com>
2023-01-17 00:08:15 +0800

    idea test git second first commit 20230117 00:08

--- src/zhangjian/test/git/TestGit.java
+++ src/zhangjian/test/git/TestGit.java
@@ -3,5 +3,6 @@ package zhangjian.test.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("test git in idea");
     }
 }
