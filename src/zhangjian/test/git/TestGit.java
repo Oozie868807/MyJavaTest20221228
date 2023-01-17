e3647a6a708a46542729f0722bf4d7560607a9b2
Oozie <goodMorning_glb@zhangjian.com>
2023-01-17 19:29:01 +0800

    idea test git pull github 1 commit 20230117 00:19

--- src/zhangjian/test/git/TestGit.java
+++ src/zhangjian/test/git/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("test git in idea");
+        System.out.println("push to github test");
     }
 }
